c0f551aa91baba75f02b0cae7ff0fd85c32571d4
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-08 15:11:17 +0530

    Create string operation.py

---
+++ hacckerrank/python/string operation.py
@@ -0,0 +1,19 @@
+#string operation 
+
+#convantention
+
+string1="hello"
+string2="fam"
+string3=string1+string2
+print(string3)
+
+string4=string1+" "+string2
+print(string4)
+
+#string operation
+string1="hello"
+string2="friends"
+string3=string1+" " +string2
+print (string3)
+    
+    

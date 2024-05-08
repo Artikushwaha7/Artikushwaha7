95a53ae5e5401cd893021d97ce0bfb104bcd9b22
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-08 15:06:02 +0530

    Create string.py

---
+++ hacckerrank/python/string.py
@@ -0,0 +1,16 @@
+# create single line string using single code
+
+string1='Hello Python'
+print(string1)
+ 
+#create single line string uaing double code 
+
+string2="learn python"
+print(string2)
+
+#create multiple line string using triple code
+ 
+string3='''Hello friends, 
+           learn something new with me 
+           and be a coder'''
+print(string3)

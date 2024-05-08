7d73ee4928343c5b90e501983287001e4d8cae90
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-08 15:14:48 +0530

    Create working and slising.py

---
+++ hacckerrank/python/working and slising.py
@@ -0,0 +1,12 @@
+#working with string
+var="helloworld"
+print (var[4])
+print(var[8])
+print(var[0])
+ 
+       
+#slising string
+var="Hello world"
+print(var[0:3])
+print(var[2:8])
+print(var[0:10])

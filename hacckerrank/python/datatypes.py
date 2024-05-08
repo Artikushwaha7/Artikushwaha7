90b5d78fb741e0c361e6fe79454bdacb87bfed87
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-08 15:07:45 +0530

    Create datatypes.py

---
+++ hacckerrank/python/datatypes.py
@@ -0,0 +1,17 @@
+#Data Types 
+     
+marks=100
+num=10.5
+char="python"
+cash=3+4j
+print(marks)
+print(num)
+print(char)
+print(cash)
+
+#to know about which data type is
+
+print(type(marks))
+print(type(num))
+print(type(char))
+print(type(cash))

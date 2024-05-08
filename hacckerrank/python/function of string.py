c2c3febcc4a75ffd4a216099d34c331b94d5b4c3
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-08 15:16:39 +0530

    Create function of string.py

---
+++ hacckerrank/python/function of string.py
@@ -0,0 +1,19 @@
+#function with strings
+#len()
+var1="apples are good"
+print(len(var1))
+
+#max()
+var2="mangoes are sweet"
+print(max(var2))
+
+#min()
+var3="mangoes are sweet"
+print(min(var3))
+
+#join()
+var4="good"
+var5="work"
+print(var4.join(var5))
+
+#

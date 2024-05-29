21e2e6128392f4ec2f0951e86acd4b5e868d09ee
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-29 15:06:48 +0530

    Create creating a list.py

---
+++ python program/creating a list.py
@@ -0,0 +1,13 @@
+#creating a list
+list_var=["apple","banana","mango"]
+print(list_var)
+
+list_int=[1,2,3,4,5]
+print(list_int)
+
+list_var=[1,"apple",2,"mango"]
+print(list_var)
+
+list_var=[1,"apple",2,"mango"]
+print(type(list_var))
+

869b7d1186bb95ce7e4ddfe65281156a6f4b4678
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-29 15:09:08 +0530

    Create Assign a list to another list.py

---
+++ python program/Assign a list to another list.py
@@ -0,0 +1,12 @@
+#Assigning a list to another list
+mobile=["Apple","Samsung","Nokia","vivo"]
+laptop=mobile 
+
+print(type(mobile))
+print(type(laptop))
+
+mobile[2]="infinix"
+
+print(mobile )
+print(laptop)
+

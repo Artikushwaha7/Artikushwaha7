5a302729e45f94b71f2da64b54bcec09d96bc0d1
Artikushwaha7 <165774926+Artikushwaha7@users.noreply.github.com>
2024-05-29 15:14:13 +0530

    Create Tuple.py

---
+++ python program/Tuple.py
@@ -0,0 +1,16 @@
+# Tuple 
+
+a=["rat","cat","bat"]
+print(tuple(a))
+
+b=[1,2,3,4,5]
+print(tuple(b))
+
+c=(1,2,3,4)
+print(tuple(c))
+
+d=("sun","moon","star")
+print(tuple(d))
+
+e=["sun",2,"cat",4]
+print(tuple(e))
